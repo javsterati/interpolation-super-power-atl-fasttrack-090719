9e307910d755d36f4d3abd224707b457f6c1d24d
Javier Huerta Jr <javsteratii@gmail.com>
2019-09-07 19:31:10 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,8 +1,5 @@
 def display_rainbow(cheese)
-   puts "R: #{cheese[0]}, O: #{cheese[1]}"
-   puts "Y: #{cheese[2]}"
-   puts "G: #{cheese[3]}"
-   puts "B: #{cheese[4]}"
+   puts "R: #{cheese[0]}, O: #{cheese[1]}, Y: #{cheese[2]}, G: #{cheese[3]}"B: #{cheese[4]}"
    puts "I: #{cheese[5]}"
    puts "V: #{cheese[6]}"
 end
